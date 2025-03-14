8eb3d6be0140193367e44b6d4df99807dd7b395b
maliharon <142988041+maliharon@users.noreply.github.com>
2025-03-14 22:44:34 +0100

    jeg har endre neo i map

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,33 +4,22 @@ import "../style/nav.scss"
 
 export default function Nav() /*Definerer og eksporterer en React-komponent kalt nav*/{
   return (/*Returenerer <nav>-tag som omslutter navigasjonsmenyen*/
-    <nav>
-    <ul /*Lager en uordnet liste <ul> for navigasjonslenkene*/> 
-      {resources.map((res, index) => /*Bruker vi map()-funksjonen
-       til å løpe resources-listen og lag en <li> for hver kategori*/  (
-        <li key={index} /*Opretter en <li> listeelement for hver kategori*/> 
-          <NavLink /*Oppretter en NavLink som brukes til navigasjon*/
-            to={`/${res.category}`} /*Setter to-attributtet slik at lenken peker til kategory-navn*/
-            className={({ isActive }) => isActive ? "link active" : "link"}
-          >
-            {res.category /*Viser kategori navnet inne i NavnLink, så brukeren ser teksten på lenken*/} 
-          </NavLink>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
-    /*
+    
+    
     <nav>
       <ul>
-        {resources.map((res, index) => (
-        <li key={index}>
+        {resources.map((res, index) => /*Bruker vi map()-funksjonen
+       til å løpe resources-listen og lag en <li> for hver kategori*/  (
+        <li key={index} /*Opretter en <li> listeelement for hver kategori*/>
           
-            <NavLink to={`/${res.category}`} className="link">{res.category}</NavLink>
+            <NavLink to={`/${res.category  /*Viser kategori navnet inne i NavnLink,
+               så brukeren ser teksten på lenken*/}`} className="link">{res.category}</NavLink>
+            
         </li> 
         ))}
       </ul>
     </nav>
+
+
   );
-   */
 }
